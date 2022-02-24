240a77e99240e47cacfe43209ca6077c677e54fe
Your Name <you@example.com>
2022-02-24 16:41:13 +0100

    Commit

--- index.js
+++ index.js
@@ -72,3 +72,24 @@ You can explore the collection at the npm website.
 One package we like is nodemon. It’s a powerful tool for development in Node that watches all
 the files in a project you’re working on, and automatically restarts your application when any of them change.
 */
+
+//Event-Driven Architecture
+console.log('-------------Event-Driven Architecture------------')
+// Here we require in the 'events' module and save a reference to it in an events variable
+let events = require('events');
+
+let listenerCallback = (data) => {
+    console.log(`Celebrate ${data}`);
+}
+
+// Here we create an instance of the EventEmitter class
+let myEmitter = new events.EventEmitter();
+
+// Here we subscribe to 'celebration' events and provide a callback function which will
+// be passed the event's data
+myEmitter.on('celebration', listenerCallback);
+
+// Here we emit an event, we pass the event type, 'celebration', as the first argument,
+// and the event data as the second
+myEmitter.emit('celebration', 'good times, come on!','\n');
+
